4e89471d7f7a1f507e648a2a1beec05cc3936a05
flo <florian.lueffe@mway.io>
2023-06-11 21:38:32 +0200

    ui update

--- frontend/src/appConfig.ts
+++ frontend/src/appConfig.ts
@@ -1,9 +1,7 @@
 export const app: { [key: string]: string } = {
-  name: 'dApp Starter', // web app name
-  title: 'dApp Starter Boilerplate', // website title
-  description: 'dApp Starter Boilerplate by https://github.com/arisac', // description of website
-  keywords: 'web3, dapp, ethereum, ethers, wagmi, rainbowkit, nextjs, react, tailwindcss, sass, boilerplate', // keywords for SEO
-  url: 'https://dapp-starter.aris.ac', // website url
+  name: 'PeerNPay', // web app name
+  title: 'PeerNPay', // website title
+  description: 'PeerNPay', // description of website
   image: '/icon-512x512.png', // image url for SEO
   favicon: '/favicon.ico', // favicon url
   twitter: '@arisdotac', // twitter account
